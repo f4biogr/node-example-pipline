8eb5d2e7ad9a2c05dfd1540c7188b1dcbca3a42a
Fábio da Rosa <fabio.gorosa@gmail.com>
2019-11-15 20:45:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.en("Hello getintodevops.com\n");
+  response.end("Hello getintodevops.com\n");
 });
 
 // listen on localhost:8000
